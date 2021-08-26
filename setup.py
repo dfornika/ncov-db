3843fef015e8925a99738c8c67196c23ea570392
Dan Fornika <dfornika@gmail.com>
2021-08-26 11:35:37 -0700

    Add tests stub

--- setup.py
+++ setup.py
@@ -1,13 +1,20 @@
-#!/usr/bin/env python
+from pathlib import Path
 
-from distutils.core import setup
+from distutils.core import setup, find_packages
+
+import store_variants
 
 setup(
     name='store_variants',
-    version='0.1.0',
+    version=store_variants.__version__,
     description='',
     author='Dan Fornika',
     author_email='dan.fornika@bccdc.ca',
     url='',
     packages=['store_variants'],
+    packages=find_packages(exclude=('tests', 'tests.*')),
+    python_requires='>=3.5',
+    install_requires=Path('requirements.txt').read_text()),
+    setup_requires=['pytest-runner', 'flake8'],
+    tests_require=Path('requirements-tests.txt').read_text()), 
 )
---
+++ tests/tests.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class TestTruism(unittest.TestCase):
+    def test_truism(self):
+        self.assertEqual(1, 1)
+
+
+if __name__ == "__main__":
+    unittest.main()
